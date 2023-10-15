86c2e67a480ae2c6d793f97fa901e1a901a67e71
BaluNeu <91381373+BaluNeu@users.noreply.github.com>
2023-10-15 18:34:10 -0400

    Time: 191 ms (39.04%), Space: 27.8 MB (52.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,24 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k%len(nums) # suppose k value is greater than len(nums)
+        k = k%len(nums) # lf k>length
         l,r = 0,len(nums)-1
-        while l<r: # loop continues untill all the elements are reversed
-            nums[l],nums[r] = nums[r],nums[l]
-            l,r = l+1,r-1
-        l,r = 0,k-1 # 1st part -- changing the left and right values and reversing
+
         while l<r:
             nums[l],nums[r] = nums[r],nums[l]
             l,r = l+1,r-1
-        l,r = k,len(nums)-1 # 2nd part 
+        l,r = 0,k-1 # 1st part
         while l<r:
             nums[l],nums[r] = nums[r],nums[l]
             l,r = l+1,r-1
-        # for i in range(k):
+        l,r = k,len(nums)-1
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l,r = l+1,r-1
 
-        #     element = nums.pop()
-        #     nums.insert(0,element)
-
-        # return nums
-
+        return nums
         
