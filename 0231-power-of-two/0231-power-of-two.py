97029d5ff8ef80fa2553911c6c6b07c4d14ca071
Bala Yeshwanth Mundlamuri <91381373+BaluNeu@users.noreply.github.com>
2024-02-19 12:06:48 -0500

    Time: 27 ms (96.66%), Space: 16.5 MB (96.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -4,10 +4,8 @@ class Solution:
             return False
         new_n = bin(n)
         new_n = int(new_n[2:])
-        print(new_n)
         if new_n % (10**(len(str(new_n)) - 1)) == 0:
             return True
-        else:
-            return False
+        return False
         
         
